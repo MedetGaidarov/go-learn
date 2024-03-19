d9c4a0578fedc20169ec167804ff77cee99be1f2
medetgaidarov <mgaiidarov@gmail.com>
2024-03-20 01:40:20 +0500

    auth was done

--- sso/internal/services/auth/auth.go
+++ sso/internal/services/auth/auth.go
@@ -2,11 +2,25 @@ package services
 
 import (
 	"context"
+	"fmt"
 	"go-learn/sso/internal/domain/models"
+	"go-learn/sso/internal/lib/logger/sl"
+	"log/slog"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserStorage interface {
 	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
+}
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
 }
 
 type UserProvider interface {
@@ -15,4 +29,67 @@ type UserProvider interface {
 
 type AppProvider interface {
 	App(ctx context.Context, appID int) (models.App, error)
+}
+
+type Auth struct {
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
+
+func New(
+	log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		usrSaver:    userSaver,
+		usrProvider: userProvider,
+		log:         log,
+		appProvider: appProvider,
+		tokenTTL:    tokenTTL, // Время жизни возвращаемых токенов
+	}
+}
+
+// RegisterNewUser registers new user in the system and returns user ID.
+// If user with given username already exists, returns error.
+
+func (a *Auth) RegisterNewUser(ctx context.Context, email string, pass string) (int64, error) {
+	// op (operation) - имя текущей функции и пакета. Такую метку удобно
+	// добавлять в логи и в текст ошибок, чтобы легче было искать хвосты
+	// в случае поломок.
+	const op = "Auth.RegisterNewUser"
+	// Создаём локальный объект логгера с доп. полями, содержащими полезную инфу
+	// о текущем вызове функции
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email),
+	)
+
+	log.Info("registering user")
+
+	// Генерируем хэш и соль для пароля.
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+
+	if err != nil {
+		log.Error("failed to generate password hash", sl.Err(err))
+		return 0, fmt.Errorf("%s : %w", op, err)
+	}
+
+	// Сохраняем пользователя в БД
+
+	id, err := a.usrSaver.SaveUser(ctx, email, passHash)
+
+	if err != nil {
+		log.Error("failed to save user", sl.Err(err))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
 }
